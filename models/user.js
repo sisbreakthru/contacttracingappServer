e40d7a4e2aa88636c4aa2c742934113ebbd85548
Linda Smith <sisbreakthru@comcast.net>
2020-07-29 21:53:43 -0400

    Authentication with Passport

---
+++ models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    // email: {
+    //     type: String,
+    //     validate: {
+    //         validator: (v) => {
+    //             return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+    //         },
+    //         message: props => `${props.value} is not a valid email address! Please fill in a valid email address.`
+    //     },
+    //     required: [true, 'Email address is required']
+    // },
+    admin: {
+        type: Boolean,
+        default: false
+    }
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', userSchema);
